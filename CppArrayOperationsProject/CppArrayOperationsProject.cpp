58a425aac43426c1e0f1366fbf09cc2a0011284e
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-01-17 21:19:19 +0300

    17-01-24

--- CppArrayOperationsProject/CppArrayOperationsProject.cpp
+++ CppArrayOperationsProject/CppArrayOperationsProject.cpp
@@ -9,6 +9,12 @@ void ArrayReverse(int array[], int size);
 void ArrayShiftLeft(int array[], int size, int steps);
 
 void ArraySortSelect(int array[], int size);
+void ArraySortBubble(int array[], int size);
+void ArraySortShacker(int array[], int size);
+void ArraySortInsert(int array[], int size);
+
+int ArraySearchLine(int array[], int size, int key);
+int ArraySearchBinary(int array[], int size, int key);
 
 int ArrayMaxValue(int array[], int size);
 int ArrayMaxIndex(int array[], int size);
@@ -35,7 +41,10 @@ int main()
     ArrayShiftLeft(array, size, 2);
     ArrayPrint(array, size);
 
-    ArraySortSelect(array, size);
+    //ArraySortSelect(array, size);
+    //ArraySortBubble(array, size);
+    //ArraySortShacker(array, size);
+    ArraySortInsert(array, size);
     ArrayPrint(array, size);
 }
 
@@ -112,3 +121,110 @@ void ArraySortSelect(int array[], int size)
         array[indexMin] = temp;
     }
 }
+
+void ArraySortBubble(int array[], int size)
+{
+    int temp;
+    bool isSort;
+
+    for (int i{}; i < size - 1; i++)
+    {
+        isSort = true;
+        for (int j{ size - 1 }; j > i; j--)
+            if (array[j] < array[j - 1])
+            {
+                temp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = temp;
+                isSort = false;
+            }
+        if (isSort)
+            break;
+    }
+        
+}
+
+void ArraySortShacker(int array[], int size)
+{
+    int top{ 0 };
+    int bottom{ size - 1 };
+
+    int temp;
+    bool isSort;
+
+    while (top < bottom)
+    {
+        isSort = true;
+        for(int i{ bottom }; i > top; i--)
+            if (array[i] < array[i - 1])
+            {
+                temp = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = temp;
+                isSort = false;
+            }
+        if (isSort)
+            break;
+        top++;
+
+        isSort = true;
+        for(int i{ top }; i < bottom; i++)
+            if (array[i] > array[i + 1])
+            {
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                isSort = false;
+            }
+        if (isSort)
+            break;
+        bottom--;
+    }
+}
+
+void ArraySortInsert(int array[], int size)
+{
+    int temp;
+    for (int i{ 1 }; i < size; i++)
+    {
+        temp = array[i];
+        int j;
+        for (j = i; j > 0; j--)
+            if (array[j - 1] > temp)
+                array[j] = array[j - 1];
+            else
+                break;
+        array[j] = temp;
+    }
+}
+
+int ArraySearchLine(int array[], int size, int key)
+{
+    for (int i{}; i < size; i++)
+        if (key == array[i])
+            return i;
+
+    return -1;
+}
+
+int ArraySearchBinary(int array[], int size, int key)
+{
+    int left{};
+    int right{ size - 1 };
+    int middle;
+
+    while (left <= right)
+    {
+        middle = (left + right) / 2;
+        if (key == array[middle])
+            return middle;
+        if (key < array[middle])
+            right = middle - 1;
+        else
+            left = middle + 1;
+    }
+
+    return -1;
+}
+
+
